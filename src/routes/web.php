a011e6c622151e6a7983af714117acddad274b5b
Ahmet Yilmaz <yilmut_-_53@hotmail.com>
2020-06-03 20:43:03 +0300

    route part fixed

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,9 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/teams','TeamController@index');
-Route::get('/teams/{team}','TeamController@show');
+Route::get('/', 'HomeController@index');
